39e955657e19a932232606a03e56e0f853d984d8
Falkirks <me@falkirks.com>
2017-04-29 08:08:55 -0700

    Minor text changes

--- private/faq.js
+++ private/faq.js
@@ -8,8 +8,8 @@ module.exports = [
         answer: "We want to provide our users with the best possible software. We are experimenting with a bunch of new features to speed up rendering of banners. We are still <b>free</b> and offer all the same features as before."
     },
     {
-        question: "I heard that the new backend will break old URLs, is this true?",
-        answer: "Yes, entirely. We will provide no redirects. There are two reasons for this: 1) We want to complete redo our backend to be faster and simpler, dropping support makes this possible. and 2) Our old server is swamped with so many requests that it can't render anything, by killing off the old URI, we will reduce the amount of requests drastucally and allow the app to scale again."
+        question: "My old URL is broken. WHY???",
+        answer: "The new backend will breaks all existing URLs. We will provide no redirects. There are two reasons for this: 1) We want to complete redo our backend to be faster and simpler, dropping support makes this possible. and 2) Our old server is swamped with so many requests that it can't render anything, by killing off the old URI, we will reduce the amount of requests drastically and allow the app to scale again."
     },
     {
         question: "What changes have been made in this new fancy backend?",
